90ae64963a50f0b965490787477758dc4f9795c8
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-12-17 00:43:11 -0500

    Update main.rs
    Added logger definition, for more advanced logs; Stores logs in the application's local data directory for now.
    Utilize tokio_util's CancellationToken and TaskTracker to allow for graceful shutdown.
    Create the server in a task spawned by the TaskTracker, utilizing WebSocketServer.
    Use tokio::signal to listen for a CTRL+C to manually trigger graceful shutdown.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,86 +1,61 @@
-#![allow(non_snake_case, non_upper_case_globals)]
+mod config;
+mod net;
 
+use std::path::PathBuf;
 use ::anyhow::Result;
-use ::fastwebsockets::{FragmentCollector, OpCode};
-use ::fastwebsockets::upgrade::{upgrade, UpgradeFut};
-use ::http_body_util::Empty;
-use ::hyper::{Request, Response};
-use ::hyper::body::{Incoming, Bytes};
-use ::hyper::server::conn::http1;
-use ::hyper::service::service_fn;
-use ::hyper_util::rt::TokioIo;
+use ::flexi_logger::{Duplicate, FileSpec, Logger};
+use ::log::{error, info};
 use ::tokio::main;
-use ::tokio::net::TcpListener;
+use ::tokio::signal;
+use ::tokio_util::task::TaskTracker;
+use ::tokio_util::sync::CancellationToken;
+use crate::config::{loadConfig, localDataPath};
+use crate::net::WebSocketServer;
 
-const BindAddress: &'static str = "127.0.0.1:7890";
+const LoggerLevel: &'static str = "info";
+const LoggerDir: &'static str = "log";
+const LoggerDefaultBaseDir: &'static str = ".";
 
 #[main(flavor = "current_thread")]
 async fn main() -> Result<()>
 {
-	let listener = TcpListener::bind(BindAddress).await?;
-	println!("Listening on {}", BindAddress);
-	loop
+	let mut logPath = PathBuf::from(LoggerDefaultBaseDir);
+	if let Some(path) = localDataPath()
 	{
-		let (stream, _) = listener.accept().await?;
-		println!("Client connected");
-		
-		tokio::spawn(async move {
-			let io = TokioIo::new(stream);
-			let connectionFuture = http1::Builder::new()
-				.serve_connection(io, service_fn(serverUpgrade))
-				.with_upgrades();
-			
-			if let Err(e) = connectionFuture.await
-			{
-				println!("An error occurred: {:?}", e);
-			}
-		});
+		logPath = PathBuf::from(path);
 	}
-}
-
-async fn handleClient(future: UpgradeFut) -> Result<()>
-{
-	let mut ws = FragmentCollector::new(future.await?);
+	logPath = logPath.join(LoggerDir);
 	
-	loop
-	{
-		let frame = ws.read_frame().await?;
-		match frame.opcode
+	Logger::try_with_str(LoggerLevel)?
+		.log_to_file(
+			FileSpec::default()
+				.directory(logPath)
+		)
+		.duplicate_to_stderr(Duplicate::Error)
+		.duplicate_to_stdout(Duplicate::Info)
+		.start()?;
+	
+	let cancelToken = CancellationToken::new();
+	let tracker = TaskTracker::new();
+	
+	let serverToken = cancelToken.clone();
+	tracker.spawn(async move {
+		if let Ok(config) = loadConfig()
 		{
-			OpCode::Close => {
-				println!("Client disconnected!");
-				break;
-			},
-			//Echo back
-			OpCode::Text | OpCode::Binary => {
-				if frame.opcode == OpCode::Text
-				{
-					match std::str::from_utf8(frame.payload.as_ref())
-					{
-						Ok(s) => println!("Input: '{}'", s),
-						Err(e) => println!("Failed to parse packet: {:?}", e),
-					}
-				}
-				
-				ws.write_frame(frame).await?;
-			},
-			_ => {},
-		}
-	}
-	
-	return Ok(());
-}
-
-async fn serverUpgrade(mut request: Request<Incoming>) -> Result<Response<Empty<Bytes>>>
-{
-	let (response, future) = upgrade(&mut request)?;
-	
-	tokio::task::spawn(async move {
-		if let Err(e) = tokio::task::unconstrained(handleClient(future)).await
-		{
-			eprintln!("Error in websocket connection: {}", e);
+			let server = WebSocketServer::from(config);
+			let _ = server.start(serverToken).await;
 		}
 	});
+	tracker.close();
 	
-	return Ok(response);
+	//Graceful shut down
+	match signal::ctrl_c().await
+	{
+		Ok(()) => cancelToken.cancel(),
+		Err(e) => error!("Error waiting for Ctrl+C: {:?}", e),
+	}
+	
+	tracker.wait().await;
+	info!("Server stopped");
+	return Ok(());
 }
