8fff278574f78fb0b3c7d9034d2650ad4b87aec7
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-12-17 15:30:50 -0500

    Update client.rs
    Abstracted pieces out into individual functions.
    Added processText to handle text payloads.
    On receiving a ClientIdentity payload, update the client's id and in the message queue as well.
    On receiving a Broadcast payload, queue a broadcast in the message queue.

--- server/src/net/client.rs
+++ server/src/net/client.rs
@@ -1,12 +1,12 @@
 use ::anyhow::Result;
-use fastwebsockets::{Frame, Payload};
-use ::fastwebsockets::{FragmentCollector, OpCode};
+use ::fastwebsockets::{FragmentCollector, Frame, Payload, OpCode};
 use ::fastwebsockets::upgrade::UpgradeFut;
 use ::hyper::upgrade::Upgraded;
 use ::hyper_util::rt::TokioIo;
 use ::log::{info, error};
-use tokio_util::sync::CancellationToken;
-use super::messager::getMessageQueue;
+use ::tokio_util::sync::CancellationToken;
+use super::payload::{Broadcast, ClientIdentity};
+use super::queue::getMessageQueue;
 
 #[derive()]
 pub struct WebSocketClient
@@ -40,55 +40,111 @@ impl WebSocketClient
 		{
 			tokio::select! {
 				_ = token.cancelled() => {
-					info!("Client loop ending");
+					info!("Graceful shutdown - Client loop ending");
 					break;
 				},
 				
 				result = self.socket.read_frame() => {
 					if let Ok(frame) = result
 					{
-						match frame.opcode
+						if self.poll(frame).await?
 						{
-							OpCode::Close => {
-								if let Ok(messager) = getMessageQueue().lock()
-								{
-									let _ = messager.removeId(self.id);
-								}
-								info!("Client disconnected!");
-								break;
-							},
-							
-							//Echo back
-							OpCode::Text | OpCode::Binary => {
-								if frame.opcode == OpCode::Text
-								{
-									match std::str::from_utf8(frame.payload.as_ref())
-									{
-										Ok(s) => info!("Input: '{}'", s),
-										Err(e) => error!("Failed to parse packet: {:?}", e),
-									}
-								}
-								
-								self.socket.write_frame(frame).await?;
-							},
-							
-							_ => {},
-						}
-						
-						let mut messages = vec![];
-						if let Ok(queue) = getMessageQueue().lock()
-						{
-							messages = queue.readMessages(self.id);
-						}
-						
-						for message in messages
-						{
-							let frame = Frame::text(Payload::Owned(message.into_bytes()));
-							self.socket.write_frame(frame).await?;
+							break;
 						}
 					}
 				}
 			}
+		}
+		
+		return Ok(());
+	}
+	
+	async fn poll(&mut self, frame: Frame<'_>) -> Result<bool>
+	{
+		match frame.opcode
+		{
+			OpCode::Close => {
+				if let Ok(messager) = getMessageQueue().lock()
+				{
+					let _ = messager.removeId(self.id);
+				}
+				info!("Client disconnected!");
+				return Ok(true);
+			},
+			
+			OpCode::Text => self.processText(frame).await?,
+			
+			/*
+			OpCode::Binary => {
+				self.socket.write_frame(frame).await?;
+			},
+			*/
+			
+			_ => {},
+		}
+		
+		self.sendQueuedMessages().await?;
+		
+		return Ok(false);
+	}
+	
+	async fn processText(&mut self, frame: Frame<'_>) -> Result<()>
+	{
+		match std::str::from_utf8(frame.payload.as_ref())
+		{
+			Ok(s) => {
+				if let Some((typeName, json)) = s.split_once("{")
+				{
+					let json = format!("{{{}", json);
+					match typeName
+					{
+						Broadcast::Name => {
+							let obj: Broadcast = serde_json::from_str(json.as_str())?;
+							if let Ok(queue) = getMessageQueue().try_lock()
+							{
+								queue.queueBroadcast(obj.text.to_owned());
+							}
+						},
+						
+						ClientIdentity::Name => {
+							let obj: ClientIdentity = serde_json::from_str(json.as_str())?;
+							
+							//TODO: Expand this to safely handle ID collisions; Probably will require actual authentication
+							//Update the id in the message queue as well
+							if let Ok(queue) = getMessageQueue().try_lock()
+							{
+								queue.removeId(self.id);
+								self.id = obj.id;
+								queue.registerId(self.id);
+							}
+							
+							let response = Frame::text(Payload::Owned(format!("Client ID set as {}", self.id).into_bytes()));
+							self.socket.write_frame(response).await?;
+						},
+						
+						_ => {},
+					};
+				}
+			},
+			
+			Err(e) => error!("Failed to parse text packet: {:?}", e),
+		}
+		
+		return Ok(());
+	}
+	
+	async fn sendQueuedMessages(&mut self) -> Result<()>
+	{
+		let mut messages = vec![];
+		if let Ok(queue) = getMessageQueue().try_lock()
+		{
+			messages = queue.readMessages(self.id);
+		}
+		
+		for message in messages
+		{
+			let frame = Frame::text(Payload::Owned(message.into_bytes()));
+			self.socket.write_frame(frame).await?;
 		}
 		
 		return Ok(());
