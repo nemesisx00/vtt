012ea41b07f2c6c83407a98072036807cefee41b
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-12-17 00:36:52 -0500

    Create server.rs
    WebSocketServer controls the main server loop which polls a TcpListener on the configured ip and port, waiting for clients to connect. Configurd for graceful shutdown.

---
+++ server/src/net/server.rs
@@ -0,0 +1,95 @@
+use ::anyhow::Result;
+use ::fastwebsockets::upgrade::{UpgradeFut, upgrade};
+use ::http_body_util::Empty;
+use ::hyper::{Request, Response};
+use ::hyper::body::{Incoming, Bytes};
+use ::hyper::server::conn::http1;
+use ::hyper::service::service_fn;
+use ::hyper_util::rt::TokioIo;
+use ::log::{error, info};
+use ::tokio::net::TcpListener;
+use tokio_util::sync::CancellationToken;
+use crate::config::Config;
+use super::client::WebSocketClient;
+
+#[derive(Clone, Default)]
+pub struct WebSocketServer
+{
+	config: Config,
+}
+
+impl From<Config> for WebSocketServer
+{
+	fn from(value: Config) -> Self
+	{
+		return Self
+		{
+			config: value.to_owned(),
+			..Default::default()
+		};
+	}
+}
+
+impl WebSocketServer
+{
+	pub async fn start(&self, token: CancellationToken) -> Result<()>
+	{
+		let listener = TcpListener::bind(self.config.network.fullAddress()).await?;
+		info!("Listening on {}", self.config.network.fullAddress());
+		
+		loop
+		{
+			let cancelToken = token.clone();
+			tokio::select! {
+				_ = token.cancelled() => break,
+				
+				result = listener.accept() => {
+					if let Ok((stream, _)) = result
+					{
+						info!("Client connected");
+						
+						tokio::spawn(async move {
+							let io = TokioIo::new(stream);
+							let connectionFuture = http1::Builder::new()
+								.serve_connection(io, service_fn(|request: Request<Incoming>| async {
+									let ct = cancelToken.clone();
+									return serverUpgrade(request, ct).await;
+								}))
+								.with_upgrades();
+							
+							if let Err(e) = connectionFuture.await
+							{
+								error!("An error occurred: {:?}", e);
+								cancelToken.cancel();
+							}
+						});
+					}
+				}
+			}
+		}
+		
+		return Ok(());
+	}
+}
+
+async fn handleClient(future: UpgradeFut, token: CancellationToken) -> Result<()>
+{
+	let mut client = WebSocketClient::fromUpgradeFut(future).await?;
+	client.start(token).await?;
+	return Ok(());
+}
+
+async fn serverUpgrade(mut request: Request<Incoming>, token: CancellationToken) -> Result<Response<Empty<Bytes>>>
+{
+	let (response, future) = upgrade(&mut request)?;
+	
+	let cancelToken = token.clone();
+	tokio::task::spawn(async move {
+		if let Err(e) = tokio::task::unconstrained(handleClient(future, cancelToken)).await
+		{
+			error!("Error in websocket connection: {}", e);
+		}
+	});
+	
+	return Ok(response);
+}
